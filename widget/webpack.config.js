ec2664f012bc967f5014d9f2b1e6653f0754ccef
anisiaap <pirvulescuanisiaa@gmail.com>
2025-04-24 22:00:46 +0300

    rename

--- widget/webpack.config.js
+++ widget/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
-
 module.exports = {
     mode: 'development',  // or 'production'
     entry: './src/index.js',
@@ -26,12 +25,10 @@ module.exports = {
                     }
                 }
             },
-            // For CSS:
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             },
-            // For images:
             {
                 test: /\.(png|jpe?g|gif|svg)$/,
                 type: 'asset/resource'
@@ -41,15 +38,21 @@ module.exports = {
     devServer: {
         port: 3030,
         host: '0.0.0.0',
-        allowedHosts: 'all',  // 👈 this line allows widget.local.easybox
+        allowedHosts: 'all',
         https: false,
         static: {
             directory: path.join(__dirname, 'public'),
         },
         open: true
-    }
-    ,
+    },
     resolve: {
         extensions: ['.js', '.jsx']
-    }
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: 'public/index.html', to: '.' }
+            ]
+        })
+    ]
 };
