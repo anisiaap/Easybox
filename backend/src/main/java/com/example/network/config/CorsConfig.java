df3974eb6be6f3a3a3e1b69583edd40ba5428a0a
anisiaap <pirvulescuanisiaa@gmail.com>
2025-04-25 12:03:58 +0300

    backend widget api

--- backend/src/main/java/com/example/network/config/CorsConfig.java
+++ backend/src/main/java/com/example/network/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://192.168.1.133:3000", "http://192.168.1.133:3001", "http://localhost:3001", "http://localhost:3000", "http://localhost:3030", "http://localhost:5500", "http://192.168.1.133:8081")); // Allow your React app
+        config.setAllowedOrigins(Arrays.asList("https://widget.easybox.ro","http://192.168.1.133:3000", "http://192.168.1.133:3001", "http://localhost:3001", "http://localhost:3000", "http://localhost:3030", "http://localhost:5500", "http://192.168.1.133:8081")); // Allow your React app
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
