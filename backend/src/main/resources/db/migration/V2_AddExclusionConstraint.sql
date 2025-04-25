9aa5a4b4891ce7bf50338a8724b9777ee71bf8b8
anisiaap <pirvulescuanisiaa@gmail.com>
2025-04-25 09:54:35 +0300

    backend env

--- backend/src/main/resources/db/migration/V2_AddExclusionConstraint.sql
+++ backend/src/main/resources/db/migration/V2_AddExclusionConstraint.sql
@@ -1,18 +1,18 @@
--- V2__AddExclusionConstraint.sql
-
 CREATE EXTENSION IF NOT EXISTS btree_gist;
 
 ALTER TABLE reservation
-  ALTER COLUMN compartment_id SET NOT NULL;
+    ALTER COLUMN compartment_id SET NOT NULL;
 
 ALTER TABLE reservation
-  ADD CONSTRAINT compartment_no_overlap
-  EXCLUDE USING gist (
-    compartment_id WITH =,
-    tsrange(reservation_start, reservation_end, '[]') WITH &&
-  );
--- ALTER TABLE reservation ADD CONSTRAINT compartment_no_overlap
---     EXCLUDE USING GIST (compartment_id WITH =, tsrange(reservation_start, reservation_end, '[]') WITH &&);
---
--- ALTER TABLE reservation ADD CONSTRAINT no_overlapping_reservations
---     EXCLUDE USING GIST (compartment_id WITH =, tsrange(reservation_start, reservation_end) WITH &&);
+    ADD CONSTRAINT compartment_no_overlap
+    EXCLUDE USING gist (
+      compartment_id WITH =,
+      tsrange(reservation_start, reservation_end, '[]') WITH &&
+    );
+
+ALTER TABLE reservation
+    ADD CONSTRAINT no_overlapping_reservations
+    EXCLUDE USING gist (
+      compartment_id WITH =,
+      tsrange(reservation_start, reservation_end) WITH &&
+    );
