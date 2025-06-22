ac69203a2b9ee5ed9b9b382d9b72434fb1e3fd91
anisiaap <pirvulescuanisiaa@gmail.com>
2025-06-23 00:25:39 +0300

    new try improve

--- admin-dashboard/src/pages/Bakeries.tsx
+++ admin-dashboard/src/pages/Bakeries.tsx
@@ -287,7 +287,7 @@ const Bakeries: React.FC = () => {
                         {/*        b.phone*/}
                         {/*    )}*/}
                         {/*</Td>*/}
-                        <Td>{c.phoneNumber}</Td>
+                        <Td>{b.phoneNumber}</Td>
                         <Td>{b.pluginInstalled ? '✅' : '❌'}</Td>
                         <Td>
                             <Button onClick={() => {
